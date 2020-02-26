ef7c6c8557b1497b9f6c480d0c4ef93970063e37
filipasenra <filipasenra4@gmail.com>
2020-02-26 10:54:06 +0000

    Error in lookup

--- I01/EchoClient.java
+++ I01/EchoClient.java
@@ -1,21 +1,105 @@
 import java.io.IOException;
 import java.net.*;
 
+enum TypeOper {
+    REGISTER, LOOKUP
+}
+
 public class EchoClient {
+
+    private static String host;
+    private static String port;
+    private static TypeOper oper;
+    private static String DNSName;
+    private static String ipAddress;
+
     public static void main(String[] args) throws IOException {
-         if (args.length != 3) {
-            System.out.println("Usage: java Echo <hostname> <port> <string to echo>");
+        if (!parseArgs(args)) {
             return;
         }
 
         // send request
         DatagramSocket socket = new DatagramSocket();
-         int port = Integer.parseInt(args[1]);
-        byte[] sbuf = args[2].getBytes();
+        int port = Integer.parseInt(args[1]);
+
+        String request = buildRequest();
+
+        byte[] sbuf = request.getBytes();
         InetAddress address = InetAddress.getByName(args[0]);
-        DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length,
-                address, port);
+        DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length, address, port);
+
         socket.send(packet);
+
+
+        //receiving response
+        byte[] rbuf = new byte[256];
+        DatagramPacket responsePacket = new DatagramPacket(rbuf, rbuf.length);
+        socket.receive(responsePacket);
+
+        // display response
+        String received = new String(responsePacket.getData());
+        System.out.println("Client: " + received);
+
+        socket.close();
+    }
+
+    private static String buildRequest() {
+
+        String request = oper + " " + DNSName;
+
+        if (oper == TypeOper.REGISTER) {
+
+            request += " " + ipAddress;
+
+        }
+
+        return request;
+
+    }
+
+    private static boolean parseArgs(String[] args) {
+
+        if (args.length < 3) {
+
+            System.out.println("Usage: java Client <host> <port> <oper> <opnd>");
+
+            return false;
+        }
+
+        host = args[0];
+        port = args[1];
+
+        if (args[2].equals("register"))
+            oper = TypeOper.REGISTER;
+        else if (args[2].equals("lookup"))
+            oper = TypeOper.LOOKUP;
+        else {
+            System.out.println("Usage: <oper> must be 'register' or 'lookup'");
+            return false;
+        }
+
+        if (oper == TypeOper.REGISTER) {
+
+            if (args.length != 5) {
+                System.out.println("Usage: Client <hostname> <port> register <DNS name> <IP address>");
+                return false;
+            }
+
+            DNSName = args[3];
+            ipAddress = args[4];
+
+        } else {
+
+            if (args.length != 4) {
+                System.out.println("Usage: Client <hostname> <port> lookup <DNS name>");
+                return false;
+            }
+
+            DNSName = args[3];
+
+        }
+
+        return true;
+
     }
 }
-
--- I01/EchoServer.java
+++ I01/EchoServer.java
@@ -1,36 +1,105 @@
 import java.io.IOException;
 import java.net.*;
+import java.util.HashMap;
 
 public class EchoServer {
+
+    private static int port;
+    private static HashMap<String, String> database = new HashMap<String, String>();
+
     public static void main(String[] args) throws IOException {
-         if (args.length != 1) {
-            System.out.println("Usage: java Echo <port>");
+
+        if (!parseArgs(args)) {
             return;
         }
 
-        int port = Integer.parseInt(args[0]);
-
         // send request
         DatagramSocket socket = new DatagramSocket(port);
-       // byte[] sbuf = args[1].getBytes();
-        InetAddress address = InetAddress.getByName(args[0]);
-        //DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length, address, 4445);
-          
+
         while (true) {
-        // get response
-        byte[] rbuf = new byte[256];
-        DatagramPacket packet = new DatagramPacket(rbuf, rbuf.length);
-        socket.receive(packet);
-        
-        // display response
-        String received = new String(packet.getData());
-        System.out.println("Echoed Message: " + received);
+            // get request
+            byte[] rbuf = new byte[256];
+            DatagramPacket packet = new DatagramPacket(rbuf, rbuf.length);
+            socket.receive(packet);
 
+            // display resquest
+            String received = new String(packet.getData());
+            System.out.println("Server: " + received);
 
-       
+            // parse response
+            String response = parseRequest(received);
+
+            // sending response
+            byte[] sbuf = response.getBytes();
+            InetAddress address = packet.getAddress();
+            int clientPort = packet.getPort();
+            DatagramPacket responsePacket = new DatagramPacket(sbuf, sbuf.length, address, clientPort);
+            socket.send(responsePacket);
+
         }
+
+        // socket.close();
+    }
+
+    private static String parseRequest(String received) {
+
+        String response = "";
+
+        String[] tokens = received.split(" ", 3);
         
-        
+        if (tokens.length != 2 && tokens.length != 3) {
+
+            System.out.println("ERROR IN RECEIVED MESSAGE");
+            System.exit(-1);
+        }
+
+        String oper = tokens[0];
+        String DNSName = tokens[1];
+
+        if (oper.equals("REGISTER")) {
+
+            if (!database.containsKey(DNSName)) {
+
+                database.put(DNSName, tokens[2]);
+                response = Integer.toString(database.size());
+
+            } else {
+                response = "-1"; // make this a MACRO
+            }
+
+        } else if (oper.equals("LOOKUP")) {
+
+            System.out.println("|" + DNSName + "|");
+
+            if (database.containsKey(DNSName)){
+
+                response = database.get(DNSName);
+                
+            } else {
+                response = "NOT_FOUND"; // make this a MACRO
+            }
+
+        } else {
+
+
+            System.out.println("ERROR IN RECEIVED MESSAGE");
+            System.exit(-1);
+        }
+
+        return response;
 
     }
+
+    private static boolean parseArgs(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Usage: java Echo <port>");
+            return false;
+        }
+
+        port = Integer.parseInt(args[0]);
+        return true;
+
+    }
+
 }
